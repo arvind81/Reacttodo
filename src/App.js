a9a7dfd0f48685d9a029ae89972409852d3659f0
arvind81 <47685213+arvind81@users.noreply.github.com>
2020-06-02 13:14:41 -0400

    secondchange

--- src/App.js
+++ src/App.js
@@ -5,11 +5,48 @@ import Todoinput from "./container/Todoinput";
 import Todolist from "./container/Todolist";
 
 export default class App extends Component {
+  state = {
+    items: [],
+    id: "",
+    item: "",
+    editItem: false,
+  };
+  handleChange = (e) => {
+    console.log("change");
+  };
+  handleSubmit = (e) => {
+    console.log("submit");
+  };
+  clearList = (e) => {
+    console.log("clear");
+  };
+  handleDelete = (e) => {
+    console.log("del");
+  };
+  handleEdit = (e) => {
+    console.log("edit");
+  };
+
   render() {
     return (
-      <div>
-        <Todoinput />
-        <Todolist />
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo-list</h3>
+            <Todoinput
+              it={this.state.it}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <Todolist
+              items={this.state.items}
+              clearList={this.clearList}
+              handleEdit={this.handleEdit}
+              handleDelete={this.handleDelete}
+            />
+          </div>
+        </div>
       </div>
     );
   }
